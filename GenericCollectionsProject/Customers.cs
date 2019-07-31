31ad046b3ee7d7892ad96a5d2653871610beee28
Briana Presley <presley.briana@gmail.com>
2019-07-31 16:08:40 -0400

    Added List

---
+++ GenericCollectionsProject/Customers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsProject {
+
+    public class Customers {
+
+        List<Customer> customerList = new List<Customer>();
+
+        public void LoadCustomers() {
+            var cust1 = new Customer { Name = "P&G", State = "OH" };
+            var cust2 = new Customer { Name = "Microsoft", State = "WA" };
+            var cust3 = new Customer { Name = "Target", State = "MN" };
+
+            customerList.Add(cust1);
+            customerList.Add(cust2);
+            customerList.Add(cust3);
+
+        }
+
+        public void PrintCustomers() { //Get each name in customer list and display
+            foreach (var cust in customerList) {
+                Console.WriteLine($"The customer name is {cust.Name} and they are located in {cust.State}");
+            }
+        }
+
+        public class Customer {
+            public string Name { get; set; }
+            public string State { get; set; }
+        }
+    }
+}
--- GenericCollectionsProject/Program.cs
+++ GenericCollectionsProject/Program.cs
@@ -1,9 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericCollectionsProject {
+
+    //Bowling - Generate 10 frames with scores ranging from 0-30 and calculate total.
+    //Use a for loop to generate 10 scores then use a foreach loop to calculate total.
+
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var customers = new Customers();
+            customers.LoadCustomers(); //to call a method from another class
+            customers.PrintCustomers();
+        }
+        static void Run() { 
+            var rnd = new Random(); //Used to generate random number
+
+            List<int> frames = new List<int>(10); //Collection has only 10 items
+
+            for(var idx = 0; idx < 10; idx++) { //load 10 more
+                var score = rnd.Next(0, 31);
+                frames.Add(score);
+            }
+
+            int total = 0;
+            foreach(var frame in frames) { // sum the scores
+                total += frame;
+            }
+
+            Console.WriteLine($"Game score is {total}");
         }
     }
 }
+
+    //MY ANSWER
+    //Random rnd = new Random();
+    //rnd.Next(1 - 31);
+    
+    //int[] frames = new int[10] {rnd}
+    
+    //for(var idx = 0; idx < frames.Length; idx++) {
+    //    total += frames[idx];
+    //}
+    
+    
+    //int total = 0;
+    //int index = 0;
+    //    while (index < 10) {
+    //        total = total + frames[index];
+    //        index = index + 1;
+    //    }
+    //    Console.WriteLine($"Total is {total}");
+
+
+    
+    //List<string> names = new List<string>()
+    
+    //var integers = new List<int>();
+    
+    //integers.Add(7);
+    //integers.Add(4);
+    //integers.Add(13);
+    
+    //integers.Remove(4);
+    
+    //integers.AddRange(new int[] { 4, 44, 444, 4444 });
+    
+    //integers[3] = 99;
+    
+    //foreach(var i in integers) {
+    //    Console.WriteLine($"i = {i}");
+    //} 
