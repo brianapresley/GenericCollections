5bff07e80cce308cf80b699bda6036bfc4c0f2ad
Briana Presley <presley.briana@gmail.com>
2019-07-31 16:27:09 -0400

    Added Dictionary

---
+++ GenericCollectionsProject/Dictionary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsProject {
+    class Dictionary { //Can create a list with unique identifiers
+
+
+
+    }
+}
